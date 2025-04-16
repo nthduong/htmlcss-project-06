be94a1012c7e7fa0dc66e2de32000acd8edf9c9d
nthduong <nguyenthaiduong17082003@gmail.com>
2025-04-16 20:37:06 +0700

    fix small issue

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const navPc = document.querySelector("#nav-pc");
 
 navTb.innerHTML = navPc.innerHTML;
 
-const playBtn = document.querySelector("#playVideo"); // đang lấy <video>
-const video = document.querySelector("#aboutVideo");
-console.log(playBtn);
-console.log(video);
-playBtn.addEventListener("click", function () {
-    video.play();
-    playBtn.style.display = "none";
-});
+// const playBtn = document.querySelector("#playVideo"); // đang lấy <video>
+// const video = document.querySelector("#aboutVideo");
+// console.log(playBtn);
+// console.log(video);
+// playBtn.addEventListener("click", function () {
+//     video.play();
+//     playBtn.style.display = "none";
+// });
 
-video.addEventListener("click", function () {
-    if (!video.paused) {
-        video.pause();
-        playBtn.style.display = "block";
-    }
-});
+// video.addEventListener("click", function () {
+//     if (!video.paused) {
+//         video.pause();
+//         playBtn.style.display = "block";
+//     }
+// });
