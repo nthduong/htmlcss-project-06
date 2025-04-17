60cc0fd441eb1e9cdf5cd6cb845e948ba6c9bff3
Nguyễn Thái Dương (HN-IID-Sales Executive) <duongnt2@runsystem.local>
2025-04-17 18:18:31 +0700

    btn project

--- main.js
+++ main.js
@@ -18,3 +18,29 @@ navTb.innerHTML = navPc.innerHTML;
 //         playBtn.style.display = "block";
 //     }
 // });
+
+const projectBtn = document.querySelector("#project-cta");
+
+const modalProject = new Popzy({
+    templateId: "project-tab",
+    closeMethods: ["overlay", "escape", "button"],
+    cssClass: ["popzy__container", "custom-class-2"],
+    // footer: true,
+});
+
+console.log(modalProject);
+// modalProject.addFooterButton("New Tab", "btn left", function (e) {
+
+// });
+
+// modalProject.addFooterButton("Cancel", "btn margin-top", function (e) {
+//     modalProject.close();
+// });
+
+projectBtn.onclick = () => {
+    modalProject.open();
+
+    const tabs = new Tabzy("#tabs", {
+        remember: true,
+    });
+};
